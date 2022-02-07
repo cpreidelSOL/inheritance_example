6fba807911c051aa33528204bc0361bf69a5eace
corneliuspreidel <cornelius.preidel@gmail.com>
2022-02-07 11:53:30 +0100

    python & cs example for vscode

---
+++ cs/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace inheritance
+{
+    abstract class Shape
+    {
+        public Shape(string name)
+        {
+            Name = name; 
+        }
+
+        public string Name;
+
+        public abstract double GetArea();
+
+        public void Print()
+        {
+            Console.Write("My name is: " + Name);
+        }
+
+    }
+
+    class Rectangle : Shape
+    {
+        public double Length;
+        public double Width;
+        
+        public Rectangle(string name, double length, double width) : base(name)
+        {
+            Length = length;
+            Width = width;
+        }
+        public override double GetArea()
+        {
+            return Length * Width;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("My name is: " + Name);
+            Console.WriteLine("My Area is: " + GetArea());
+        }
+    }
+
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+            var rec = new Rectangle("Test_Rec", 2, 4);
+            rec.Print();
+        }
+    }
+}
---
+++ py/inheritance_example.py
@@ -0,0 +1,22 @@
+import sys
+
+
+class BuildingElement: 
+    def __init__(self, fname):
+        self.name = fname
+    
+    def printname(self):
+        print('My name is = ' + self.name)
+    
+    
+class Wall(BuildingElement): 
+    
+        def __init__(self, fname, fthickness):
+            super().__init__(fname)
+            self.thickness = fthickness
+            
+        def printname(self):
+                print('My name is = ' + self.name + ' und meine Dicke ist ' + str(self.thickness))
+                
+x = Wall("Wand", 1.12)
+x.printname()
